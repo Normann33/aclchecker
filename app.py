09fd6bc718d60761448087ca8ad06d3cb645cae7
Normann <40455681+Normann33@users.noreply.github.com>
2024-11-28 11:19:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -170,4 +170,4 @@ def about():
 
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, ssl_context=('ac.net.rts-cert.pem', 'ac.net.rts-key.pem'))
+    socketio.run(app, host='0.0.0.0', port=5000, ssl_context=('cert.pem', 'key.pem'))
